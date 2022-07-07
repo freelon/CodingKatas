d1d9f83f9638a9632f1a9b99ab244ba15ec66a26
Johannes <freelon@gmx.net>
2022-07-07 19:42:01 +0200

    ..

--- to-dictionary/src/main/java/de/torks/katas/todictionary/DictionaryUtil.java
+++ to-dictionary/src/main/java/de/torks/katas/todictionary/DictionaryUtil.java
@@ -6,6 +6,14 @@ public class DictionaryUtil {
 
     public static Map<String, String> toDictionary(String input) {
 
+        int splitIndex = input.indexOf("=");
+        if (splitIndex >= 0) {
+
+            String left = input.substring(0, splitIndex);
+            String right = input.substring(splitIndex + 1);
+            return Map.of(left, right);
+        }
+
         return Map.of();
     }
 }
--- to-dictionary/src/test/java/de/torks/katas/todictionary/DictionaryFromStringTest.java
+++ to-dictionary/src/test/java/de/torks/katas/todictionary/DictionaryFromStringTest.java
@@ -2,6 +2,9 @@ package de.torks.katas.todictionary;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DictionaryFromStringTest {
@@ -10,5 +13,11 @@ public class DictionaryFromStringTest {
     void testEmpty() {
 
         assertTrue(DictionaryUtil.toDictionary("").isEmpty());
+    }
+
+    @Test
+    void testSingle() {
+
+        assertEquals(Map.of("a", "=1"), DictionaryUtil.toDictionary("a==1"));
     }
 }
