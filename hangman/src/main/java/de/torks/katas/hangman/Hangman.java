c629b9e43bf4f79b794e59cd0c5fb62d5769fefa
Johannes <freelon@gmx.net>
2022-07-14 16:48:48 +0200

    Hangman..

--- hangman/src/main/java/de/torks/katas/hangman/Hangman.java
+++ hangman/src/main/java/de/torks/katas/hangman/Hangman.java
@@ -1,4 +1,40 @@
 package de.torks.katas.hangman;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class Hangman {
+
+    private final String word;
+    private final Set<Character> guessed = new HashSet<>();
+
+    public Hangman(String word) {
+
+        this.word = word;
+    }
+
+    public String current() {
+        return reveal();
+    }
+
+    public String guess(char character) {
+        guessed.add(character);
+        return reveal();
+    }
+
+    private String reveal() {
+        return word.chars()
+                .map(this::revealedWord)
+                .mapToObj(Character::toString)
+                .collect(Collectors.joining());
+    }
+
+    private int revealedWord(int c) {
+        char character = (char) c;
+        if (guessed.contains(Character.toLowerCase(character)) || guessed.contains(Character.toUpperCase(character)))
+            return c;
+        else
+            return '_';
+    }
 }
--- hangman/src/test/java/de/torks/katas/hangman/HangmanTest.java
+++ hangman/src/test/java/de/torks/katas/hangman/HangmanTest.java
@@ -2,11 +2,45 @@ package de.torks.katas.hangman;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class HangmanTest {
 
     @Test
-    void testGuessedWordCorrect() {
+    void testInitialIsEmpty() {
 
-        Hangman hangman = new Hangman();
+        String word = "foobar";
+        Hangman hangman = new Hangman(word);
+
+        assertEquals("______", hangman.current());
+    }
+
+    @Test
+    void testFittingCharRevealsItself() {
+
+        String word = "foobar";
+        Hangman hangman = new Hangman(word);
+
+        assertEquals("_oo___", hangman.guess('o'));
+        assertEquals("foo___", hangman.guess('f'));
+    }
+
+    @Test
+    void testUnfittingCharDoesntReveal() {
+
+        String word = "foobar";
+        Hangman hangman = new Hangman(word);
+
+        assertEquals("______", hangman.guess('x'));
+    }
+
+    @Test
+    void testIgnoresCapitalization() {
+
+        String word = "foobaR";
+        Hangman hangman = new Hangman(word);
+
+        assertEquals("___b__", hangman.guess('B'));
+        assertEquals("___b_R", hangman.guess('r'));
     }
 }
